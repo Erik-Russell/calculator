6c028da7a86bebbdc007fde7aee7f794b2184b31
Erik Russell <erik.z.russell@gmail.com>
2024-08-18 09:23:48 -0700

    js basic functions created

--- script.js
+++ script.js
@@ -3,3 +3,27 @@
 const add = function (a, b) {
     return a + b;
 };
+
+const subtract = function (a, b) {
+    return a - b;
+}
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    return a / b;
+}
+
+const reciprocal = function (a) {
+    return 1 / a;
+}
+
+const square = function (a) {
+    return a ** 2;
+}
+
+const root = function (a) {
+    return a ** (1 / 2);
+}
