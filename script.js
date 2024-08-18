1bf5a395811b58195160b2fa94b3876e116000c0
Erik Russell <erik.z.russell@gmail.com>
2024-08-18 10:06:18 -0700

    js created and tested clear, add, and equals functions

--- script.js
+++ script.js
@@ -34,62 +34,90 @@ const percentage = function (a) {
 
 // display
 
-const displayBox = document.querySelector('.display');
+const displayMain = document.querySelector('.display-main');
+const displayHistory = document.querySelector('.display-history')
 
 // number listeners
 
 const oneButton = document.querySelector('#one');
 oneButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 1;
+    displayMain.textContent = displayMain.textContent += 1;
     console.log('oneButton');
 })
 const twoButton = document.querySelector('#two');
 twoButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 2;
+    displayMain.textContent = displayMain.textContent += 2;
     console.log('twoButton');
 })
 const threeButton = document.querySelector('#three');
 threeButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 3;
+    displayMain.textContent = displayMain.textContent += 3;
     console.log('threeButton');
 })
 const fourButton = document.querySelector('#four');
 fourButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 4;
+    displayMain.textContent = displayMain.textContent += 4;
     console.log('fourButton');
 })
 const fiveButton = document.querySelector('#five');
 fiveButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 5;
+    displayMain.textContent = displayMain.textContent += 5;
     console.log('fiveButton');
 })
 const sixButton = document.querySelector('#six');
 sixButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 6;
+    displayMain.textContent = displayMain.textContent += 6;
     console.log('sixButton');
 })
 const sevenButton = document.querySelector('#seven');
 sevenButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 7;
+    displayMain.textContent = displayMain.textContent += 7;
     console.log('sevenButton');
 })
 const eightButton = document.querySelector('#eight');
 eightButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 8;
+    displayMain.textContent = displayMain.textContent += 8;
     console.log('eightButton');
 })
 const nineButton = document.querySelector('#nine');
 nineButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 9;
+    displayMain.textContent = displayMain.textContent += 9;
     console.log('nineButton');
 })
 const zeroButton = document.querySelector('#zero');
 zeroButton.addEventListener('click', () => {
-    displayBox.textContent = displayBox.textContent += 0;
+    displayMain.textContent = displayMain.textContent += 0;
     console.log('zeroButton');
 })
 // const decimalButton = document.querySelector('#decimal');
 // decimalButton.addEventListener('click', () => {
-//     displayBox.textContent = displayBox.textContent += 
+//     displayMain.textContent = displayMain.textContent += 
 //     console.log('decimalButton');
 // })
+
+// function listeners
+
+let currentFunction = '';
+
+const clearButton = document.querySelector('#clear-all');
+clearButton.addEventListener('click', () => {
+    displayHistory.textContent = '';
+    displayMain.textContent = '';
+    console.log('clearButton');
+})
+
+const addButton = document.querySelector('#add');
+addButton.addEventListener('click', () => {
+    currentFunction = 'add';
+    displayHistory.textContent = displayMain.textContent;
+    displayMain.textContent = '';
+    console.log('addButton');
+})
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', () => {
+    let result = add(Number(displayHistory.textContent), Number(displayMain.textContent));
+    displayHistory.textContent = displayHistory.textContent + "+" + displayMain.textContent;
+    displayMain.textContent = result;
+    console.log('equalsButton');
+})
